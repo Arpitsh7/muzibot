3424b76be438bc6cc9e5bb18f6800ef14c2be003
Arpit Sharma <officialarpit7@gmail.com>
2026-02-08 02:12:23 +0530

    Update signin page UI

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -33,77 +33,84 @@ export default function Signin() {
 
   // redirect after login
   useEffect(() => {
-    async function checkRole() {
-      const res = await fetch("/api/role")
-      const data = await res.json()
-
-      if (!data.authenticated) return
-
-      if (data.isDriver) {
-        router.push("/driver/dashboard")
-      } else {
-        router.push("/rider/dashboard")
-      }
-    }
-
     if (!isPending && session) {
-      checkRole()
+      router.push("/dashboard")
     }
   }, [session, isPending, router])
 
   return (
-    <div className="w-full min-h-screen flex justify-center items-center bg-gray-50">
-      <form
-        onSubmit={handleSignin}
-        className="w-full max-w-md px-8 py-14 bg-gray-50 text-center"
-      >
-        <Heading />
+    <div className="min-h-screen w-full relative bg-white">
+      {/* Purple Glow Top */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background: "#ffffff",
+          backgroundImage: `
+            radial-gradient(
+              circle at top center,
+              rgba(173, 109, 244, 0.5),
+              transparent 70%
+            )
+          `,
+          filter: "blur(80px)",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
 
-        <div className="mx-auto my-4 flex flex-col gap-8">
-          <FormField label="Email" dot="yes">
-            <Input
-              type="email"
-              placeholder="enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              autoComplete="email"
-            />
-          </FormField>
+      {/* Content */}
+      <div className="w-full min-h-screen flex justify-center items-center relative z-10">
+        <form
+          onSubmit={handleSignin}
+          className="w-full max-w-md px-8 py-14 bg-gray-50 text-center shadow-xl rounded-lg"
+        >
+          <Heading />
 
-          <FormField label="Password" dot="yes">
-            <Input
-              type="password"
-              placeholder="enter password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              autoComplete="current-password"
-            />
-          </FormField>
+          <div className="mx-auto my-4 flex flex-col gap-8">
+            <FormField label="Email" dot="yes">
+              <Input
+                type="email"
+                placeholder="enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                autoComplete="email"
+              />
+            </FormField>
 
-          {error && <p className="text-red-500 text-sm">{error}</p>}
+            <FormField label="Password" dot="yes">
+              <Input
+                type="password"
+                placeholder="enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                autoComplete="current-password"
+              />
+            </FormField>
 
-          <div className="mt-4">
-            <button
-              type="submit"
-              disabled={loading}
-              className="w-full bg-black transition-all duration-150 text-white px-4 py-3 rounded-md cursor-pointer hover:-translate-y-0.5 active:scale-98 after:content-[''] after:w-1/2 after:h-[400px] after:absolute after:bg-white/20 relative overflow-hidden after:-left-20 after:-top-20 after:rotate-10 after:-translate-x-20 hover:after:translate-x-[200%] after:backdrop-blur-[0.5px] after:transition-all after:duration-1000 disabled:opacity-50 disabled:cursor-not-allowed"
-            >
-              {loading ? "Signing in..." : "Sign In Now"}
-            </button>
+            {error && <p className="text-red-500 text-sm">{error}</p>}
+
+            <div className="flex flex-col gap-4 mt-4">
+              <button
+                type="submit"
+                disabled={loading}
+                className="bg-black transition-all duration-150 text-white px-4 py-3 rounded-md cursor-pointer hover:-translate-y-0.5 active:scale-98 after:content-[''] after:w-1/2 after:h-[400px] after:absolute after:bg-white/20 relative overflow-hidden after:-left-20 after:-top-20 after:rotate-10 after:-translate-x-20 hover:after:translate-x-[200%] after:backdrop-blur-[0.5px] after:transition-all after:duration-1000 disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                {loading ? "Signing in..." : "Sign In Now"}
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
 
 function Heading() {
   return (
-    <h1 className="text-4xl font-bold tracking-tighter bg-clip-text bg-gradient-to-b from-neutral-800 to-neutral-600 selection:bg-black selection:text-white">
+    <h1 className="text-4xl font-bold tracking-tighter text-neutral-600 selection:bg-black selection:text-white">
       Welcome{" "}
-      <span className="relative z-10 inline-block text-white after:absolute after:inset-0 after:-z-10 after:h-full after:w-full after:-skew-10 after:bg-red-500">
+      <span className="relative ml-1 mr-1 pb-1.5 pr-2 pl-1 pt-1 z-10 inline-block text-white after:absolute after:inset-0 after:-z-10 after:h-full after:w-full after:-skew-10 after:bg-red-500">
         back
       </span>
     </h1>
@@ -123,7 +130,9 @@ function FormField({
     <div className="flex flex-col gap-2 text-left">
       <label
         className={
-          dot ? "after:ml-0.5 after:text-red-500 after:content-['*']" : ""
+          dot
+            ? " text-neutral-300 font-display after:ml-0.5 after:text-red-500 after:content-['*']"
+            : ""
         }
       >
         {label}
@@ -137,7 +146,7 @@ function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      className="rounded-xl bg-gray-100 p-4 placeholder:text-neutral-300 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-orange-200 focus:offset-2"
+      className="text-neutral-700 font-display rounded-xl bg-gray-100 p-4 placeholder:text-neutral-400 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-orange-200 focus:offset-2"
     />
   )
 }
